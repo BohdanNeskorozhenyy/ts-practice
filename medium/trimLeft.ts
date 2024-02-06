d8d93a8d4dc927918c00ac95625215789112056d
Bohdan <bogdaneskor@gmail.com>
2024-02-06 20:48:08 +0200

    add TrimLeft

---
+++ medium/trimLeft.ts
@@ -0,0 +1,8 @@
+
+// Implement TrimLeft<T> which takes an exact string type and returns a new string with the whitespace beginning removed.
+
+type Space = ' ' | '\n' | '\t'
+
+type TrimLeft<S extends string> = S extends `${Space}${infer R}` ? TrimLeft<R> : S
+
+type trimed = TrimLeft<'  Hello World  '> // expected to be 'Hello World  '
